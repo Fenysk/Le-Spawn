945fe9c308e05128fafd768519385e05c3ead7d1
Fenysk <minefenysk@gmail.com>
2024-03-25 17:00:29 +0100

    🚧 Settings skeleton

--- apps/frontend/pages/parametres/index.vue
+++ apps/frontend/pages/parametres/index.vue
@@ -1,4 +1,7 @@
 <script setup lang='ts'>
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 definePageMeta({
     middleware: ['auth']
 })
@@ -17,13 +20,55 @@ onMounted(() => {
             <p class="text-zinc-700">{{ currentQuote }}</p>
             <h1 class="mt-2 text-2xl">Paramètres</h1>
         </header>
+
         <main>
             <p>C'est ici que vous pouvez modifier vos paramètres</p>
 
-            <section>
-                This is my section
-            </section>
+            <Tabs default-value="profile" class="w-full">
+                <TabsList>
+                    <TabsTrigger value="profile">
+                        Profile
+                    </TabsTrigger>
+                    <TabsTrigger value="personalInformations">
+                        Informations personnelles
+                    </TabsTrigger>
+                </TabsList>
+
+                <TabsContent value="profile">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle class="font-acephimere">Profile</CardTitle>
+                            <CardDescription>
+                                Change your profile
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent class="space-y-2">
+                            <SettingTabProfile />
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Save profile</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="personalInformations">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle class="font-acephimere">Informations personnelles</CardTitle>
+                            <CardDescription>
+                                Change your personal informations
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent class="space-y-2">
+                            <SettingTabPersonalInformations/>
+                        </CardContent>
+                        <CardFooter>
+                            <Button>Save personal informations</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+            </Tabs>
         </main>
+
         <footer>
             This is my footer
         </footer>
