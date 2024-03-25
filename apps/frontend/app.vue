1f75675edeca8af4dbb999162c83d07e4b6e3358
Fenysk <minefenysk@gmail.com>
2024-03-25 15:40:43 +0100

    💄 add border to dev/beta label

--- apps/frontend/app.vue
+++ apps/frontend/app.vue
@@ -15,7 +15,7 @@ const NODE_ENV = useRuntimeConfig().public.NODE_ENV;
 
 <template>
     <NuxtLayout>
-        <div class="fixed -right-14 bottom-1 z-[9999] -rotate-[30deg] bg-secondary px-16 py-1 font-semibold drop-shadow-flat">
+        <div class="fixed -right-14 bottom-1 z-[9999] -rotate-[30deg] border border-black bg-secondary px-16 py-1 font-semibold drop-shadow-flat" >
             {{ NODE_ENV === 'development' ? 'DEV' : 'BETA' }}
         </div>
         <NuxtPage />
