96041c3cc4e417c6b516ec3eb33bf1dfabdca02f
Fenysk <minefenysk@gmail.com>
2024-04-02 12:45:40 +0200

    💄 Add drawer skeleton on game details

---
+++ apps/frontend/components/Collection/Item/VideoGame/Card/Details.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { VideoGame } from '~/services/collections.service';
+
+const loading = ref(false);
+
+defineProps<{
+    videoGame: VideoGame;
+}>();
+</script>
+
+<template>
+    <div>
+        <pre class="max-h-[70vh] w-full overflow-auto">{{ videoGame }}</pre>
+    </div>
+</template>
